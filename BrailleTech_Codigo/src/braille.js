48eebf207226f9acbf67244eb51f15916b7a47e8
jonathanpoaquiza <poaquizajonathan75@gmail.com>
2024-05-30 19:48:08 -0500

    Update 3-braille.js
    Validations for questions and exclamations and refactoring of functions

--- BrailleTech_Codigo/src/braille.js
+++ BrailleTech_Codigo/src/braille.js
@@ -28,34 +28,113 @@ class Braille {
      * @param {string} braille - La cadena de Braille a convertir.
      * @return {string} El Braille convertido en texto.
      */
+
+  
+    // static toText(braille) {
+    //     const reversedMap = Object.fromEntries(Object.entries(brailleMap).map(([k, v]) => [v, k]));
+    //     let text = '';
+    //     let isCapital = false;
+    //     let isNumber = false;
+
+    //     for (let char of braille) {
+    //         if (char === '⠨') {
+    //             isCapital = true;
+    //         } else if (char === '⠼') {
+    //             isNumber = true;
+    //         } else {
+    //             let translatedChar = reversedMap[char] || char;
+    //             if (isCapital) {
+    //                 translatedChar = translatedChar.toUpperCase();
+    //                 isCapital = false;
+    //             }
+    //             if (isNumber) {
+    //                 if ('⠁⠃⠉⠙⠑⠋⠛⠓⠊⠚'.includes(char)) {
+    //                     translatedChar = reversedMap[char];
+    //                 } else {
+    //                     isNumber = false;
+    //                 }
+    //             }
+    //             text += translatedChar;
+    //         }
+    //     }
+    //     return text;
+    // }
+
+    static reversedMap = Object.fromEntries(Object.entries(brailleMap).map(([k, v]) => [v, k]));
+    
     static toText(braille) {
-        const reversedMap = Object.fromEntries(Object.entries(brailleMap).map(([k, v]) => [v, k]));
         let text = '';
         let isCapital = false;
         let isNumber = false;
-
-        for (let char of braille) {
-            if (char === '⠠') {
+    
+        braille.split('').forEach((char, index) => {
+            const prevChar = index > 0 ? braille[index - 1] : null;
+            const nextChar = index < braille.length - 1 ? braille[index + 1] : null;
+    
+            if (this.isCapitalIndicator(char)) {
                 isCapital = true;
-            } else if (char === '⠼') {
+            } else if (this.isNumberIndicator(char)) {
                 isNumber = true;
+            } else if (this.isPunctuation(char)) {
+                text += this.handlePunctuation(char, prevChar, nextChar, isNumber);
             } else {
-                let translatedChar = reversedMap[char] || char;
-                if (isCapital) {
-                    translatedChar = translatedChar.toUpperCase();
-                    isCapital = false;
-                }
-                if (isNumber) {
-                    if ('⠁⠃⠉⠙⠑⠋⠛⠓⠊⠚'.includes(char)) {
-                        translatedChar = reversedMap[char];
-                    } else {
-                        isNumber = false;
-                    }
-                }
-                text += translatedChar;
+                text += this.translateChar(char, isCapital, isNumber);
+                isCapital = false;
+                isNumber = this.updateIsNumber(char, isNumber);
+            }
+        });
+    
+        return text;
+    }
+    
+    static isCapitalIndicator(char) {
+        return char === '⠨';
+    }
+    
+    static isNumberIndicator(char) {
+        return char === '⠼';
+    }
+    
+    static isPunctuation(char) {
+        return char === '⠢' || char === '⠖';
+    }
+    
+    static handlePunctuation(char, prevChar, nextChar, isNumber) {
+        if (char === '⠢') { // Pregunta
+            if (!prevChar || /[.\s]/.test(prevChar)) {
+                return '¿';
+            } else {
+                return '?';
+            }
+        } else if (char === '⠖') { // Exclamación o símbolo +
+            if (isNumber || (nextChar && /[0-9]/.test(this.reversedMap[nextChar]))) {
+                return '+';
+            } else if (!prevChar || /[.\s]/.test(prevChar)) {
+                return '¡';
+            } else {
+                return '!';
             }
         }
-        return text;
+        return '';
+    }
+    
+    static translateChar(char, isCapital, isNumber) {
+        let translatedChar = this.reversedMap[char] || char;
+        
+        if (isCapital) {
+            translatedChar = translatedChar.toUpperCase();
+        }
+        if (isNumber && '⠁⠃⠉⠙⠑⠋⠛⠓⠊⠚'.includes(char)) {
+            translatedChar = Object.keys(brailleMap).find(key => brailleMap[key] === char && !isNaN(key));
+        }
+        return translatedChar;
+    }
+    
+    static updateIsNumber(char, isNumber) {
+        if (!'⠁⠃⠉⠙⠑⠋⠛⠓⠊⠚'.includes(char)) {
+            return false;
+        }
+        return isNumber;
     }
 }
 
